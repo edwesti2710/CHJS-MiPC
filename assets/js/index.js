7e73ac7c8ea76fddf803067aab3da726b88828d8
Edward Espinoza <edwesti2710@gmail.com>
2022-09-25 22:00:45 -0500

    optimization & add libraries

--- assets/js/index.js
+++ assets/js/index.js
@@ -35,17 +35,17 @@ const allProducts = [
     { id: 2, category: 'procesador', name: 'Ryzen 5 3400g', socket: 'AM4', brand: 'amd', img: 'https://www.amd.com/system/files/styles/992px/private/2019-07/238593-ryzen-5G-pib-right-facing-1260x709.png?itok=0seLB30Y', price0: 159, finalPrice() { return Math.round((this.price0 * valorDolar) * ganancia); } },
     { id: 3, category: 'procesador', name: 'Core i3 10100F', socket: 'LGA1200', brand: 'intel', img: 'https://www.intel.com/content/dam/products/hero/foreground/badge-10th-gen-core-i3-1x1.png', price0: 119, finalPrice() { return Math.round((this.price0 * valorDolar) * ganancia); } },
     { id: 4, category: 'procesador', name: 'Core i5 10400F', socket: 'LGA1200', brand: 'intel', img: 'https://www.intel.com/content/dam/products/hero/foreground/badge-10th-gen-core-i5-1x1.png', price0: 169, finalPrice() { return Math.round((this.price0 * valorDolar) * ganancia); } },
-    { id: 5, category: 'motherboard', name: 'ASUS PRIME A320M-K', socket: 'AM4', brand: 'asus', img: 'https://www.asus.com/media/global/products/KRyCoH4XfPYSg5da/P_setting_xxx_0_90_end_500.png', price0: 50, finalPrice() { return Math.round((this.price0 * valorDolar) * ganancia); } },
-    { id: 6, category: 'motherboard', name: 'ASUS PRIME B450M-A', socket: 'AM4', brand: 'asus', img: 'https://www.asus.com/media/global/products/Mh7JcLMTVjDCnQbz/P_setting_xxx_0_90_end_500.png', price0: 70, finalPrice() { return Math.round((this.price0 * valorDolar) * ganancia); } },
-    { id: 7, category: 'motherboard', name: 'ASUS PRIME B460M-A', socket: 'LGA1200', brand: 'asus', img: 'https://www.asus.com/media/global/products/sfalkfod5w5uqkjk/P_setting_xxx_0_90_end_500.png', price0: 60, finalPrice() { return Math.round((this.price0 * valorDolar) * ganancia); } },
-    { id: 8, category: 'motherboard', name: 'ASUS PRIME H410M-E', socket: 'LGA1200', brand: 'asus', img: 'https://www.asus.com/media/global/products/s2i96l2bqhnw1mhz/P_setting_xxx_0_90_end_500.png', price0: 45, finalPrice() { return Math.round((this.price0 * valorDolar) * ganancia); } },
+    { id: 5, category: 'motherboard', name: 'PRIME A320M-K', socket: 'AM4', brand: 'asus', img: 'https://www.asus.com/media/global/products/KRyCoH4XfPYSg5da/P_setting_xxx_0_90_end_500.png', price0: 50, finalPrice() { return Math.round((this.price0 * valorDolar) * ganancia); } },
+    { id: 6, category: 'motherboard', name: 'PRIME B450M-A', socket: 'AM4', brand: 'asus', img: 'https://www.asus.com/media/global/products/Mh7JcLMTVjDCnQbz/P_setting_xxx_0_90_end_500.png', price0: 70, finalPrice() { return Math.round((this.price0 * valorDolar) * ganancia); } },
+    { id: 7, category: 'motherboard', name: 'PRIME B460M-A', socket: 'LGA1200', brand: 'asus', img: 'https://www.asus.com/media/global/products/sfalkfod5w5uqkjk/P_setting_xxx_0_90_end_500.png', price0: 60, finalPrice() { return Math.round((this.price0 * valorDolar) * ganancia); } },
+    { id: 8, category: 'motherboard', name: 'PRIME H410M-E', socket: 'LGA1200', brand: 'asus', img: 'https://www.asus.com/media/global/products/s2i96l2bqhnw1mhz/P_setting_xxx_0_90_end_500.png', price0: 45, finalPrice() { return Math.round((this.price0 * valorDolar) * ganancia); } },
     { id: 9, category: 'motherboard', name: 'A320M-S2H', socket: 'AM4', brand: 'gigabyte', img: 'https://static.gigabyte.com/StaticFile/Image/Global/c9d47f21dec878e822db72d94922ee27/Product/20983/png/500', price0: 45, finalPrice() { return Math.round((this.price0 * valorDolar) * ganancia); } },
     { id: 10, category: 'motherboard', name: 'B450M DS3H', socket: 'AM4', brand: 'gigabyte', img: 'https://static.gigabyte.com/StaticFile/Image/Global/035cd59de1ee82f0105e4633da728352/Product/21100/png/500', price0: 55, finalPrice() { return Math.round((this.price0 * valorDolar) * ganancia); } },
     { id: 11, category: 'motherboard', name: 'B460M DS3H V2', socket: 'LGA1200', brand: 'gigabyte', img: 'https://static.gigabyte.com/StaticFile/Image/Global/c40c2736695f2128f767e2db209cacfa/Product/27255/png/500', price0: 50, finalPrice() { return Math.round((this.price0 * valorDolar) * ganancia); } },
     { id: 12, category: 'motherboard', name: 'H410M H', socket: 'LGA1200', brand: 'gigabyte', img: 'https://static.gigabyte.com/StaticFile/Image/Global/e24671ffd4e1db2b626ea6977d254e17/Product/25114/png/500', price0: 40, finalPrice() { return Math.round((this.price0 * valorDolar) * ganancia); } },
 ]
 // Variables de Usuario
-let carrito = [];
+carrito = [];
 
 // Recuperando datos del carrito
 let carritoJSON = localStorage.getItem('carritoJSON');
@@ -83,37 +83,13 @@ categoriasHTML.innerHTML = categoriasInner;
 drawCards(allProducts);
 
 function filterbyC(categoria) {
-    let productsHTML = document.querySelector('.cards--container');
-    let productsInner = '';
     let brands = [];
     const productosFiltrados = allProducts.filter(producto => producto.category === categoria);
     productosFiltrados.forEach(producto => {
         brands.push(producto.brand);
         brands = [...new Set(brands)];
-
-        productsInner += `<article class="card">
-        <div class="imgContainer">
-            <div class="popUp">
-            </div>
-            <img src="${producto.img}" alt="${producto.name}">
-        </div>
-        <div class="card--data">
-            <h3>${producto.brand.toUpperCase()} - ${producto.name}</h3>
-            <div class="score">
-                <i class="fa-solid fa-star gold"></i>
-                <i class="fa-solid fa-star gold"></i>
-                <i class="fa-solid fa-star gold"></i>
-                <i class="fa-solid fa-star"></i>
-                <i class="fa-solid fa-star"></i>
-            </div>
-        </div>
-        <button title="Agregar al carrito" category="${categoria}" brand="${producto.brand}" id="${producto.id}" onclick="addToCart('${producto.id}')" class="addToCart">
-            <h4>${localMoneda} ${producto.finalPrice()}<i class="fa-solid fa-cart-plus"></i></h4>
-        </button>
-    </article>`
     })
-    productsHTML.innerHTML = productsInner;
-
+    drawCards(productosFiltrados);
     brandsHTML = document.querySelector('.brands__ul')
     brandsInner = '';
     brands.forEach(brand => {
@@ -138,7 +114,7 @@ function drawCards(array) {
             <img src="${producto.img}" alt="${producto.name}">
         </div>
         <div class="card--data">
-            <h3>${producto.brand.toUpperCase()} - ${producto.name}</h3>
+            <h3>${producto.brand.toUpperCase()} ${producto.name.toUpperCase()}</h3>
             <div class="score">
                 <i class="fa-solid fa-star gold"></i>
                 <i class="fa-solid fa-star gold"></i>
@@ -163,10 +139,9 @@ function addToCart(id) {
     } else {
         carrito.push(allProducts.find(obj => obj.id == id))
         productoSeleccionado = carrito.find(obj => obj.id == id)
+        // const getCount = parseInt(productoSeleccionado.carrito) || 0
         let getCount = parseInt(productoSeleccionado.carrito);
-        if (isNaN(getCount)) {
-            productoSeleccionado.carrito = 0;
-        }
+        isNaN(getCount) === true && (productoSeleccionado.carrito = 0);
         productoSeleccionado.carrito++;
     }
     drawCarrito();
@@ -175,14 +150,14 @@ function addToCart(id) {
     showToastfyAlert('Agregado al carrito', 2000)
 }
 
-function removeFromCart(id){
-    carrito.splice(carrito.indexOf(carrito.find(obj => obj.id == id)),1)
+function removeFromCart(id) {
+    carrito.splice(carrito.indexOf(carrito.find(obj => obj.id == id)), 1)
     drawCarrito();
     updateCartData();
     saveOnLocalStorage();
 }
 
-function saveOnLocalStorage(){
+function saveOnLocalStorage() {
     carritoJSON = JSON.stringify(carrito);
     localStorage.setItem('carritoJSON', carritoJSON);
 }
@@ -233,6 +208,20 @@ function calcularPreciototal() {
         return costoTotal;
     }
     document.querySelector(".cartTotal").innerHTML = `<h4>Total:</h4>
-    <h4>${localMoneda} ${operaciones()}</h4>`
-        ;
+    <h4>${localMoneda} ${operaciones()}</h4><button id="btnPagar">Pagar</button>`;
+    let btnPagar = document.getElementById('btnPagar');
+    btnPagar.onclick = () => {
+        compraSatisfactoria()
+    };
+}
+
+// Accion al pagar
+function compraSatisfactoria() {
+    Swal.fire({
+        title: 'La compra se realizó correctamente!',
+        icon: 'success',
+        confirmButtonText: 'Aceptar',
+    }).then((result) => {
+        result['isConfirmed'] && ((localStorage.clear())( location.reload()))
+    })
 }
